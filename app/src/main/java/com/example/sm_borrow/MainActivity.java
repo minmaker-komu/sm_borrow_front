751d8979b3ebec8ba8c26f8e718833807381e482
choiminsun <backwonand@sookmyung.ac.kr>
2024-12-17 05:43:35 +0900

    오류 수정

--- app/src/main/java/com/example/sm_borrow/MainActivity.java
+++ app/src/main/java/com/example/sm_borrow/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.sm_borrow;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
